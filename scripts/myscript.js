4b63c6dc8109f1c62aff30b0b935e187ae2fbfd9
dlstess <zahary1981@gmail.com>
2017-02-25 13:26:24 +0200

    2

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,3 +1,10 @@
 /**
  * Created by Dlstess on 25.2.2017 г..
  */
+function subimt() {
+    let user=$('#username').val();
+    let pass=$('#password').val();
+
+    console.log(user);
+    console.log(pass);
+}
